2fb7c3170df4803d56c1389e5cf1b59787b23176
bisubus <bisubus@users.noreply.github.com>
2025-07-23 18:28:48 +0300

    (examples) Add await using disposal example

--- examples/jest.config.base.js
+++ examples/jest.config.base.js
@@ -5,15 +5,20 @@
 module.exports = {
  clearMocks: true,
  testEnvironment: 'node',
- roots: ['<rootDir>/src', '<rootDir>/test'],
+ roots: ['<rootDir>/src'],
  testPathIgnorePatterns: ['/node_modules/', '/~~', '~~/'],
  modulePathIgnorePatterns: ['/~~', '~~/'],
+ moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+ moduleNameMapper: {
+ '^@shared/(.+)$': '<rootDir>/../_shared/$1',
+ },
  transform: {
  '^.+\\.tsx?$': ['ts-jest', {
  tsconfig: {
  target: 'es2020',
  module: 'commonjs',
  moduleResolution: 'node',
+ lib: ['es2020'],
  esModuleInterop: true,
  strict: true,
  },
