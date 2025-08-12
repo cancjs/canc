2bc81873e27e252a95b9402f61adf6553580e758
bisubus <bisubus@users.noreply.github.com>
2025-08-12 10:16:11 +0300

    (decorators) Add legacy and babel-legacy entry points

--- packages/canc-decorators/rollup.config.js
+++ packages/canc-decorators/rollup.config.js
@@ -1,3 +1,7 @@
-import { createConfigs } from '../../rollup.config.base.js';
+import { createMultiConfigs } from '../../rollup.config.base.js';
 
-export default createConfigs({ name: 'canc_decorators' });
+export default createMultiConfigs([
+ { input: 'src/index.ts', base: 'index' },
+ { input: 'src/legacy.ts', base: 'legacy' },
+ { input: 'src/babel-legacy.ts', base: 'babel-legacy' },
+], { name: 'canc_decorators' });
