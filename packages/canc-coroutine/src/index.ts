82d08b776a4ae179754cc1c4b2b64ffe108a33c0
bisubus <bisubus@users.noreply.github.com>
2026-05-18 05:51:25 +0300

    (coroutine) Add iter subpath + mirror namespace exports

--- packages/canc-coroutine/src/index.ts
+++ packages/canc-coroutine/src/index.ts
@@ -4,4 +4,4 @@ export * from './coroutine-iter';
 // lets consumers do `import * as coroutine from '@cancjs/coroutine'; coroutine.async(...)`.
 // Named-import form needs an alias (`import { async as cancAsync } from '@cancjs/coroutine'`),
 // legal but awkward, document in README.
-export { cancAsync as async, cancAwait as await } from './coroutine';
+export { cancAsync as async, cancAwait as await, cancForAwait as forAwait } from './coroutine';
