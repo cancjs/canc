83e8ed0df4276f9cd828756062ff251319bc212d
bisubus <bisubus@users.noreply.github.com>
2026-07-19 15:07:44 +0300

    (examples) Remove react mobx

--- examples/app-react-mobx/src/main-canc.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Watchlist } from './Watchlist-canc';
-
-createRoot(document.getElementById('root')!).render(
- <StrictMode>
- <Watchlist />
- </StrictMode>
-);
