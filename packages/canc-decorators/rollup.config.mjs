47a8d6d68a67214052958cb8132a9921794abee9
bisubus <bisubus@users.noreply.github.com>
2026-08-02 23:45:20 +0300

    (build) Give each decorators bundle a distinct global

--- packages/canc-decorators/rollup.config.mjs
+++ packages/canc-decorators/rollup.config.mjs
@@ -1,10 +1,7 @@
 import { createMultiConfigs } from '../../rollup.config.base.js';
 
-export default createMultiConfigs(
-  [
-    { input: 'src/index.ts', base: 'index' },
-    { input: 'src/legacy.ts', base: 'legacy' },
-    { input: 'src/babel-legacy.ts', base: 'babel-legacy' },
-  ],
-  { name: 'canc_decorators' },
-);
+export default createMultiConfigs([
+  { input: 'src/index.ts', base: 'index', name: 'canc_decorators' },
+  { input: 'src/legacy.ts', base: 'legacy', name: 'canc_decorators_legacy' },
+  { input: 'src/babel-legacy.ts', base: 'babel-legacy', name: 'canc_decorators_babel_legacy' },
+]);
