fc4c932f19ffc9a98296792a810bda308311a461
bisubus <bisubus@users.noreply.github.com>
2026-08-02 23:55:34 +0300

    (repo) Untrack a scratch source file

--- packages/canc-promise/src/cancelable-promise-test~~.ts
+++
@@ -1,6 +0,0 @@
-﻿
-// var cp= _CancelablePromise.resolve(1).then().finally()//.then(a => a);
-
-// var p = Promise.all([1, Promise.resolve('s')]).then(r => r);
-
-// var p = _CancelablePromise.all([1, Promise.resolve('s')]).then(r => r);
